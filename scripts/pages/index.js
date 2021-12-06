e798476c3ec316a65baf53ac2c2bc3cdf63ea528
Kazarinov Yanek <kazarinov.yanek@gmail.com>
2021-12-06 15:10:22 +0100

    récupérer les données de json

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,15 @@
-    async function getPhotographers() {
+     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+
+        const photographers = await fetch('data/photographers.json')
+        .then(response => response.json())
+
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+          return photographers
     }
 
     async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
+        const photographersSection = document.querySelector(".photographer_section"); 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,2 @@
 //Mettre le code JavaScript lié à la page photographer.html
+// Поместите код JavaScript, связанный со страницей photographer.html
