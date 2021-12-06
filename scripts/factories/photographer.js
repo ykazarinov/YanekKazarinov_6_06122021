835ec1959d3eae0a4e47c7c425a031c63a38e31f
Kazarinov Yanek <kazarinov.yanek@gmail.com>
2021-12-06 17:38:44 +0100

    add data (country, city, tagline, price) to the user + css for it

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data; 
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        let content = `
+            <a href="#"><img alt="" src="${picture}"></a>
+            <h2><a href="#">${name}</a></h2>
+            <div class="infoPlus">
+                <div class="place">${city}, ${country}</div>
+                <div class="tagline">${tagline}</div>
+                <div class="price">${price}€/jour</div>
+            </div>
+        `
+
+        article.innerHTML = content
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
 }
+
+
